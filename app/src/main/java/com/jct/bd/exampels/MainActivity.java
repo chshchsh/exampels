8f8191bd0d0fb2e6fe801fae9da68bf698fad96a
bdelkayam <bdelkayam@gmail.com>
2018-11-19 18:14:52 +0200

    Initial commit

---
+++ app/src/main/java/com/jct/bd/exampels/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jct.bd.exampels;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
